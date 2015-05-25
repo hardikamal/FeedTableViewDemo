1b446e9daec505dcd0b3f74fa63d384f2b6dcacf
Tulsi Gajara <tulsigajara@Tulsis-Mac-mini.local>
2015-05-25 16:20:02 +0530

    Initial Commit

---
+++ Feeds/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Feeds
+//
+//  Created by Tulsi Gajara on 5/25/15.
+//  Copyright (c) 2015 Hardik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
